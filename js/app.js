d29a408b357c63ce90bd3946cba2acf29246e25b
Jim <jim@advancebass.com>
2019-10-17 22:41:40 -0700

    organize and comment code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
-/*
- * SVG ArtBoard to append elements to
- */
+/* ==========================================================================
+   Main SVG element where svg element will be appended
+   ========================================================================== */
 const artBoard = document.getElementById("artboard");
-
 const artBoardWidth = artBoard.clientWidth;
 const artBoardHeight = artBoard.clientHeight;
 
-/*
- * a utility function to help center elements horizontally
- */
+/* ==========================================================================
+   Functions
+   ========================================================================== */
+
+/* Function to help center elements horizontally
+   ========================================================================== */
 function centerX(shapeWidth) {
   return artBoardWidth / 2 - shapeWidth / 2;
 }
 
-// a utility function to help center elements vertically
+/* Function to help center elements vertically
+   ========================================================================== */
 function centerY(shapeHeight) {
   return artBoardHeight / 2 - shapeHeight / 2;
 }
 
-/*
- * use a mousedown event to create svg elements at the mouse screen position
- */
+function rotate() {}
+
+/* ==========================================================================
+   Examples
+   ========================================================================== */
+
+/* Example of using a mousedown event to create svg elements
+   ========================================================================== */
 // artBoard.addEventListener("mousedown", drawOnClick);
 
 // function drawOnClick(event) {
@@ -28,17 +36,15 @@ function centerY(shapeHeight) {
 //   circle.draw(event.offsetX, event.offsetY);
 // }
 
-/*
- * use a loop to create svg elements
- */
+/* Example of using a loop to create svg elements
+   ========================================================================== */
 // for(let i = 0; i < 500; i+=50){
 //   const circle = new Circle(20, "black");
 //   circle.draw(i, 100);
 // }
 
-/*
- * creating elements line by line
- */
+/* Example of creating elements line by line
+   ========================================================================== */
 // const box1 = new Rect(300, 400, "skyblue");
 // box1.draw(0, 0);
 
